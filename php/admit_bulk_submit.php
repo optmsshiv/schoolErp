2a33124f793e5740b68bcaa2033f318ff96bb8ee
optmsshiv <optmsshiv@gmail.com>
2024-11-02 23:00:14 +0530

    php updated

--- php/admit_bulk_submit.php
+++ php/admit_bulk_submit.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 header('Content-Type: application/json');
 
@@ -39,7 +41,16 @@ if (!$stmt) {
 
 // Bind parameters and execute for each row of data
 foreach ($data as $row) {
-    // Make sure the keys match the column names and types
+    // Check if all required keys are set
+    if (!isset($row['serial_number'], $row['first_name'], $row['last_name'], $row['phone'],
+                $row['email'], $row['date_of_birth'], $row['gender'], $row['class_name'],
+                $row['category'], $row['religion'], $row['guardian'], $row['handicapped'],
+                $row['father_name'], $row['mother_name'], $row['roll_no'], $row['sr_no'],
+                $row['pen_no'], $row['aadhar_no'], $row['admission_no'], $row['admission_date'],
+                $row['day_hosteler'])) {
+        die(json_encode(['error' => 'Missing required fields in data']));
+    }
+
     $stmt->bind_param("isssssssssssssssssssss",
         $row['serial_number'],
         $row['first_name'],
