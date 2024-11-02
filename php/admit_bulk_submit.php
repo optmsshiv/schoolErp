1db92758e142eb8b9c4380e1d378eac9d61321dc
optmsshiv <optmsshiv@gmail.com>
2024-11-02 15:14:02 +0530

    php code update

--- php/admit_bulk_submit.php
+++ php/admit_bulk_submit.php
@@ -3,6 +3,7 @@ header('Content-Type: application/json');
 
 // Enable error reporting for debugging
 ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 // Database connection parameters
