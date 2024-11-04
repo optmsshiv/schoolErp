7548fbd6eda081e821c43366ed554a7aedf048e3
optmsshiv <optmsshiv@gmail.com>
2024-11-04 09:41:15 +0530

    restor code

--- php/fetch_active_student.php
+++ php/fetch_active_student.php
@@ -33,12 +33,12 @@ if ($result->num_rows > 0) {
         echo "<tr>
                 <td><input type='checkbox'></td>
                 <td>" . $sr_no++ . "</td> <!-- Serial Number -->
-                <td>" . $row["first_name"] . "</td>
-                <td>" . $row["father_name"] . "</td>
-                <td>" . $row["class_name"] . "</td>
-                <td>" . $row["roll_no"] . "</td>
-                <td>" . $row["phone"] . "</td>
-                <td>" . $row["user_id"] . "</td>
+                <td>" . htmlspecialchars($row["first_name"]) . "</td>
+                <td>" . htmlspecialchars($row["father_name"]) . "</td>
+                <td>" . htmlspecialchars($row["class_name"]) . "</td>
+                <td>" . htmlspecialchars($row["roll_no"]) . "</td>
+                <td>" . htmlspecialchars($row["phone"]) . "</td>
+                <td>" . htmlspecialchars($row["user_id"]) . "</td>
                 <td>
                     <button class='btn btn-primary btn-sm'>Edit</button>
                     <button class='btn btn-danger btn-sm'>Delete</button>
