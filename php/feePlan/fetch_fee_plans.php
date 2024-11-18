99c9663e1b2a671d1855e77852d01477438c2a10
optmsshiv <optmsshiv@gmail.com>
2024-11-18 22:19:02 +0530

    chk

--- php/feePlan/fetch_fee_plans.php
+++ php/feePlan/fetch_fee_plans.php
@@ -4,7 +4,9 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 // Include database connection
-require_once '../db_connection.php';
+include '../db_connection.php';
+
+header('Content-Type: application/json');
 
 // Initialize response array
 $response = [
