4c19e35cabf28eb8f0ba7b19d38b4beef8802956
optmsshiv <optmsshiv@gmail.com>
2024-11-18 09:06:00 +0530

    again changes

--- php/feePlan/insert_fee_plan.php
+++ php/feePlan/insert_fee_plan.php
@@ -4,17 +4,17 @@ include '../db_connection.php'; // Include database connection file
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     try {
         // Get the form data
-        $fee_head_id = $_POST['feeHeadSelect'];
+        $fee_head_name = $_POST['feeHeadSelect'];
         $class_name = $_POST['class'];
         $month = $_POST['month'];
         $fee_amount = $_POST['feeAmount'];
 
         // Insert into the fee plan table
-        $sql = "INSERT INTO FeePlans (fee_head_id, class_name, month, fee_amount)
-                VALUES (:fee_head_id, :class_name, :month, :fee_amount)";
+        $sql = "INSERT INTO FeePlans (fee_head_name, class_name, month, fee_amount)
+                VALUES (:fee_head_name, :class_name, :month, :fee_amount)";
 
         $stmt = $pdo->prepare($sql);
-        $stmt->bindParam(':fee_head_id', $fee_head_id);
+        $stmt->bindParam(':fee_head_name', $fee_head_name);
         $stmt->bindParam(':class_name', $class_name);
         $stmt->bindParam(':month', $month);
         $stmt->bindParam(':fee_amount', $fee_amount);
