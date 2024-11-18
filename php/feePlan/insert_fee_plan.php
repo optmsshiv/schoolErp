60362884f5908ea9413145253cd388a258ae85e2
optmsshiv <optmsshiv@gmail.com>
2024-11-18 10:01:51 +0530

    insert update

--- php/feePlan/insert_fee_plan.php
+++ php/feePlan/insert_fee_plan.php
@@ -3,12 +3,12 @@ include 'db_connection.php';
 
 $feeHead = $_POST['feeHead'];
 $className = $_POST['className'];
-$months = $_POST['months']; // Comma-separated string of months
+$monthName = $_POST['monthName']; // Comma-separated string of months
 $amount = $_POST['amount'];
 
 // Insert the fee plan into the database
-$query = "INSERT INTO feePlans (fee_head_id, class_id, months, fee_amount)
-          VALUES ('$feeHead', '$className', '$months', '$amount')";
+$query = "INSERT INTO feePlans (fee_head_id, class_id, monthName, fee_amount)
+          VALUES ('$feeHead', '$className', '$monthName', '$amount')";
 
 if (mysqli_query($conn, $query)) {
     echo json_encode(['status' => 'success']);
