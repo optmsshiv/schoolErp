ed4954b4a1d91e194556d137bd8f65683eb80d92
optmsshiv <optmsshiv@gmail.com>
2024-11-20 01:04:52 +0530

    js

--- custome js/fee_plan.js
+++ custome js/fee_plan.js
@@ -420,7 +420,14 @@ document.addEventListener('DOMContentLoaded', function () {
                             `).join('')}
                         </select>
                         <label>Month Name</label>
-                        <input id="editMonth" class="swal2-input" type="text" value="${feePlan.month_name}">
+                        <select id="editMonth" class="swal2-select">
+                            ${Array.from(monthSelect.options).map(option => `
+                                <option value="${option.value}" ${option.value === feePlan.month_name ? 'selected' : ''}>
+                                    ${option.text}
+                                </option>
+                            `).join('')}
+                        </select>
+                        
                         <label>Amount</label>
                         <input id="editAmount" class="swal2-input" type="number" value="${feePlan.amount}">
                     `,
