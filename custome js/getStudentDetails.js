8d4808c0aee1accc747e5a5bc572b73888a544bf
optmsshiv <optmsshiv@gmail.com>
2024-11-22 11:21:34 +0530

    correct php path updated

--- custome js/getStudentDetails.js
+++ custome js/getStudentDetails.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   if (userId) {
       // Fetch student data from the backend
-      fetch(`../collectFeeStudentDetails/getStudentDetails.php?user_id=${userId}`)
+      fetch(`../studentInfo/getStudentDetails.php?user_id=${userId}`)
           .then(response => response.json())
           .then(data => {
               if (data.error) {
