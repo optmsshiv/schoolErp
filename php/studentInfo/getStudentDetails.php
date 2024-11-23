756bf9fa87e1fc85d85f68f63e033ac8f171cdea
optmsshiv <optmsshiv@gmail.com>
2024-11-23 22:10:11 +0530

    check updates

--- php/studentInfo/getStudentDetails.php
+++ php/studentInfo/getStudentDetails.php
@@ -4,6 +4,12 @@ include '../db_connection.php'; // Include your database connection
 if (isset($_GET['user_id'])) {
     $user_id = $_GET['user_id'];
 
+     // Check if the user_id is not empty
+     if (empty($user_id)) {
+      echo json_encode(["error" => "Invalid user_id"]);
+      exit();
+  }
+
     $sql = "SELECT * FROM students WHERE user_id = ?";
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("s", $user_id);
