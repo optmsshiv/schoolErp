9453a4ee7b4e090ecb326253305790a80b83c449
optmsshiv <optmsshiv@gmail.com>
2024-11-28 00:20:06 +0530

    js update

--- custome js/collection_page_fee_head.js
+++ custome js/collection_page_fee_head.js
@@ -20,7 +20,7 @@ function fetchFeePlansData() {
       }
 
       const months = [
-        'April', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec', 'Jan', 'Feb', 'March'
+        'April', 'May', 'June', 'July', 'August', 'September', 'Oct', 'Nov', 'Dec', 'Jan', 'Feb', 'March'
       ];
 
       // Generate the table header dynamically
