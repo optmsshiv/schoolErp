d63ff711b98d52e2bee711107b08da394d116ab5
optmsshiv <optmsshiv@gmail.com>
2024-12-05 02:03:10 +0530

    chck now

--- custome js/canvas_drop_down.js
+++ custome js/canvas_drop_down.js
@@ -1,23 +1,4 @@
- // Fetch Feeheads from the server
- fetch('../php/feeCanva/fetch_canva_feeHead.php')
- .then(response => response.json())
- .then(data => {
-   const feeTypeDropdown = document.getElementById('feeType');
-   feeTypeDropdown.innerHTML = '<option value="" disabled selected>Select Fee Type</option>'; // Clear default option
 
-   // Populate options dynamically
-   data.forEach(feehead => {
-     const option = document.createElement('option');
-     option.value = feehead.id;
-     option.textContent = feehead.fee_head_name;
-     feeTypeDropdown.appendChild(option);
-   });
- })
- .catch(error => {
-   console.error('Error fetching fee types:', error);
-   const feeTypeDropdown = document.getElementById('feeType');
-   feeTypeDropdown.innerHTML = '<option value="" disabled selected>Error loading fee types</option>';
- });
 
  /*
 // Handle Save Fee button click
--- custome js/send_and_disaplay_canva_table.js
+++ custome js/send_and_disaplay_canva_table.js
@@ -1,3 +1,25 @@
+ // Fetch Feeheads from the server
+ fetch('../php/feeCanva/fetch_canva_feeHead.php')
+ .then(response => response.json())
+ .then(data => {
+   const feeTypeDropdown = document.getElementById('feeType');
+   feeTypeDropdown.innerHTML = '<option value="" disabled selected>Select Fee Type</option>'; // Clear default option
+
+   // Populate options dynamically
+   data.forEach(feehead => {
+     const option = document.createElement('option');
+     option.value = feehead.id;
+     option.textContent = feehead.fee_head_name;
+     feeTypeDropdown.appendChild(option);
+   });
+ })
+ .catch(error => {
+   console.error('Error fetching fee types:', error);
+   const feeTypeDropdown = document.getElementById('feeType');
+   feeTypeDropdown.innerHTML = '<option value="" disabled selected>Error loading fee types</option>';
+ });
+
+ // send data to table
 document.addEventListener("DOMContentLoaded", function () {
   const saveFeeButton = document.getElementById("saveFeeButton");
 
