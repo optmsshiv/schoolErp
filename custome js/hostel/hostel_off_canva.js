6179833806585c39a9e3b58d47c6d9ef8a34126f
optmsshiv <optmsshiv@gmail.com>
2024-12-21 09:11:03 +0530

    restore

--- custome js/hostel/hostel_off_canva.js
+++ custome js/hostel/hostel_off_canva.js
@@ -1,19 +1,10 @@
 document.getElementById('loadOffcanvas').addEventListener('click', function () {
-    // Get the user_id from the URL
-    const userId = new URLSearchParams(window.location.search).get('user_id');
-
     // Dynamically load offcanvas content if needed
     fetch('/html/hostel_canva.html')
         .then(response => response.text())
         .then(html => {
             const container = document.getElementById('offcanvasContainerHostel');
             container.innerHTML = html;
-
-            // Pre-fill the hidden input field with the user_id
-            const hiddenInput = container.querySelector('#studentIdInput');
-            if (hiddenInput) {
-                hiddenInput.value = userId;
-            }
 
             // Initialize offcanvas only after it's in the DOM
             const offcanvasElement = document.getElementById('hostelCanvas');
