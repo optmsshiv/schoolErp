546de61f8deef5b6bf0107a709055baa4c3b1674
optmsshiv <optmsshiv@gmail.com>
2024-12-26 21:38:15 +0530

    check this

--- custome js/collection_page_fee_head.js
+++ custome js/collection_page_fee_head.js
@@ -257,7 +257,7 @@ const payableAmountField = document.getElementById('payableAmount');
  // Update payable amount based on concession fee
   document.getElementById('concessionFee').addEventListener('input', function () {
     // Get the updated total amount from the payableAmount input field
-    const totalAmountFromTable = initialPayableAmount;
+    let totalAmountFromTable = parseFloat(payableAmountField.value).toFixed(2) || 0;
 
     // Get the concession fee
     const concessionFee = parseFloat(this.value) || 0;
