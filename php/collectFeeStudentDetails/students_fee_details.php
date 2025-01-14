30bfe2a4ab9cc2fc4661e947dab68bd4756ac317
optmsshiv <optmsshiv@gmail.com>
2025-01-14 10:54:51 +0530

    now check

--- php/collectFeeStudentDetails/students_fee_details.php
+++ php/collectFeeStudentDetails/students_fee_details.php
@@ -18,7 +18,7 @@ try {
     $summaryQuery = "
          SELECT
         COALESCE(SUM(fd.received_amount), 0) AS total_paid_amount,
-        COALESCE(SUM(fd.due_amount), 0) AS pending_amount,
+        COALESCE(SUM(fd.pending_amount), 0) AS pending_amount,
         COALESCE(h.hostel_fee, 0) AS hostel_amount,
         COALESCE(t.transport_fee, 0) AS transport_amount
     FROM
