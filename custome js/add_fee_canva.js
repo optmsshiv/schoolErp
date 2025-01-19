0ce01b470d2b72cb9f7e9af9a0be0f3f698ceaed
optmsshiv <optmsshiv@gmail.com>
2025-01-19 21:19:48 +0530

    fee canva updated

--- custome js/add_fee_canva.js
+++ custome js/add_fee_canva.js
@@ -1,12 +1,14 @@
-fetch('../html/add_fee_canvas.html')
-  .then(response => {
-    if (!response.ok) throw new Error('HTML Load Failed');
-    return response.text();
-  })
-  .then(html => {
-    document.getElementById('canvas-container').innerHTML = html;
-  })
-  .catch(error => {
-    console.error('Error loading canvas:', error);
-    Swal.fire('Error', 'Failed to load the fee canvas. Please try again.', 'error');
-  });
+document.addEventListener("DOMContentLoaded", function() {
+  fetch("../html/add_fee_canvas.html")
+      .then(response => {
+          if (!response.ok) throw new Error('HTML Load Failed');
+          return response.text();
+      })
+      .then(html => {
+          document.getElementById('canvas-container').innerHTML = html;
+      })
+      .catch(error => {
+          console.error('Error loading canvas:', error);
+          Swal.fire('Error', 'Failed to load the fee canvas. Please try again.', 'error');
+      });
+})
