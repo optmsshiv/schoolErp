76c9e9d254a71ae15ba48cb5281109d8eab75c70
optmsshiv <optmsshiv@gmail.com>
2025-01-23 18:23:05 +0530

    now check

--- custome js/card_count/total_student_card.js
+++ custome js/card_count/total_student_card.js
@@ -3,8 +3,8 @@
         url: '/php/cardPhp/total_student_card.php', // Path to the PHP file
         method: 'GET',
         success: function(response) {
-          if (response.total_drivers !== undefined) {
-            $('#total_student').text(response.total_student.toLocaleString()); // Update the total drivers count
+          if (response.total_student !== undefined) {
+            $('#total_student').text(response.total_student.toLocaleString()); // Update the total student count
           } else {
             $('#total_student').text('Error');
           }
