ecbd7c13e1ead6f3b8778f6580b608bd923fb026
optmsshiv <optmsshiv@gmail.com>
2025-02-07 17:46:22 +0530

    `Fixed typo in manageUser.js: 'full_name' -> 'fullName'`

--- model/manageUser.js
+++ model/manageUser.js
@@ -348,7 +348,7 @@ $(document).ready(function () {
 
          if (userRow.length > 0) {
            // Update the table row data dynamically
-           userRow.find('td:nth-child(3) h6').text(full_name);
+           userRow.find('td:nth-child(3) h6').text(fullName);
            userRow.find('td:nth-child(4)').text(role);
            userRow.find('td:nth-child(5)').text(phone);
            userRow.find('td:nth-child(6)').text(formatDate(joiningDate));
