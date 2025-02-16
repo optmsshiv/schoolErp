a022078786536fb5ce272646e80bd9c96a1a37f6
optmsshiv <optmsshiv@gmail.com>
2025-02-16 21:29:44 +0530

    `Removed dropdown menu from offCanvas HTML template`

--- model/offCanvas.js
+++ model/offCanvas.js
@@ -194,7 +194,6 @@ document.addEventListener('DOMContentLoaded', function () {
         }"></a>
         <a href="javascript:;" class="tf-icons bx bx-dots-vertical-rounded bx-sm text-warning dropdown-toggle"
            data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" title="More Options"></a>
-        <div class="dropdown-menu dropdown-menu-end">${dropdownMenu}</div>
       </td>
     `;
 
