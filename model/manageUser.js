4700a76845a662143c76142deb85c4407f7168f1
optmsshiv <optmsshiv@gmail.com>
2025-02-18 23:19:07 +0530

    `Update event listeners for user view button in manageUser.js`

--- model/manageUser.js
+++ model/manageUser.js
@@ -587,8 +587,8 @@ $(function () {
 
           setTimeout(() => {
             $(document)
-              .off('click', '.userView')
-              .on('click', '.userView', function () {
+              .off('click', '#userView')
+              .on('click', '#userView', function () {
                 var userId = $(this).data('id');
                 alert('View user profile: ' + userId);
               });
@@ -635,21 +635,11 @@ $(function () {
   }
 
   // Handel 'ViewButton' click event
-/*
+
   $(document).on('click', '#userView', function () {
     var userId = $(this).data('id');
     alert('View user profile:' + userId);
   });
-*/
-
-$(document)
-  .off('click', '.userView')
-  .on('click', '.userView', function () {
-    var userId = $(this).data('id');
-    alert('View user profile: ' + userId);
-  });
-
-
 
 
   // Handle 'Credential send' button click event
