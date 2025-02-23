e0dc0c69a6db8eda10b869fbca37e3afd1342e3d
optmsshiv <optmsshiv@gmail.com>
2025-02-23 11:53:48 +0530

    `Added dropdown menu and removed inline HTML for user delete button and dropdown toggle`

--- model/offCanvas.js
+++ model/offCanvas.js
@@ -191,10 +191,10 @@ document.addEventListener('DOMContentLoaded', function () {
                   <a href="javascript:;" class="tf-icons bx bx-trash bx-sm me-2 text-danger userDelete" id="userDelete" data-id="${
                     user.user_id
                   }" title="Delete User"></a>
-
+                   <div class="dropdown">
                   <a href="javascript:;" class="tf-icons bx bx-dots-vertical-rounded bx-sm me-2 text-warning"
                     data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" title="More Options"></a>
-                  <div class="dropdown-menu dropdown-menu-end">${dropdownMenu}</div>
+                  <div class="dropdown-menu dropdown-menu-end">${dropdownMenu}</div></div>
                 </td>
             `;
           tbody.appendChild(row);
