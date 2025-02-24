48bb65a747db9b9665e6b49371121d4e6fa1ad45
optmsshiv <optmsshiv@gmail.com>
2025-02-24 09:27:42 +0530

    "Removed event listener for fetching user list on page load"

--- model/offCanvas.js
+++ model/offCanvas.js
@@ -352,7 +352,7 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
   // ✅ Load Data on Page Load
-  document.addEventListener('DOMContentLoaded', () => fetchUserList());
+  // document.addEventListener('DOMContentLoaded', () => fetchUserList());
 
   // add new user to table row
 
