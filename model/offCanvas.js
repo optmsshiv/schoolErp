827434a3fa4422e48b4bd050e165004720a899a1
optmsshiv <optmsshiv@gmail.com>
2025-02-24 10:55:18 +0530

    `Update offCanvas.js to display a loading spinner and text`

--- model/offCanvas.js
+++ model/offCanvas.js
@@ -157,7 +157,12 @@ document.addEventListener('DOMContentLoaded', function () {
       tbody = document.createElement('tbody');
       userTable.appendChild(tbody);
     }
-    tbody.innerHTML = `<tr><td colspan="8" class="text-center">Loading...</td></tr>`;
+    tbody.innerHTML = `<tr><td colspan="8" class="text-center">
+    <div id="loadingSpinner" class="spinner-border text-primary" role="status" style="display: none;">
+                    <span class="visually-hidden">Loading...</span>
+                  </div>
+    Loading...
+    </td></tr>`;
 
     fetch(`/php/userRole/get_user_role.php?page=${page}&limit=${limit}&search=${encodeURIComponent(search)}`) // Replace with your actual API endpoint
       .then(response => {
