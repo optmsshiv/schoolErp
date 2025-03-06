324a345a30536bd3bc62d8f06d76de44ac697021
optmsshiv <optmsshiv@gmail.com>
2025-03-06 17:07:40 +0530

    `Fixed payment status badge condition in search_students.js`

--- custome js/search_students.js
+++ custome js/search_students.js
@@ -204,9 +204,9 @@ async function fetchFeeDetails(userId) {
           <td align="center">₹ ${detail.advanced_amount || '0'}</td>
           <td align="center">₹ ${detail.received_amount || '0'}</td>
           <td align="center">₹ ${detail.total_amount || '0'}</td>
-          <td><span class="badge rounded-pill ${detail.status === 'Paid' ? 'bg-label-success' : 'bg-label-danger'}">${
-          detail.payment_status
-        }</span></td>
+          <td><span class="badge rounded-pill ${
+            detail.payment_status === 'Paid' ? 'bg-label-success' : 'bg-label-danger'
+          }">${detail.payment_status}</span></td>
           <td align="center">
             <div class="dropdown">
               <button class="btn text-muted p-0" type="button" data-bs-toggle="dropdown">
