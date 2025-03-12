ab2151b8b9b2284261dd73285a53946375e6c9b3
optmsshiv <optmsshiv@gmail.com>
2025-03-12 23:44:53 +0530

    `Update Endroid\QrCode usage to version 6`

--- php/require/generate-qr.php
+++ php/require/generate-qr.php
@@ -5,7 +5,7 @@ ini_set('display_errors', 1);
 
 require $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
 
-use Endroid\QrCode\QrCode;
+use Endroid\QrCode\Builder\Builder;
 use Endroid\QrCode\Writer\PngWriter;
 use Endroid\QrCode\Encoding\Encoding;
 use Endroid\QrCode\ErrorCorrectionLevel;
@@ -27,15 +27,15 @@ if ($amount <= 0) {
 // Generate the UPI payment URI
 $upi_uri = "upi://pay?pa=$upi_id&pn=School%20Fees&mc=0000&tid=123456&tr=ABC123&tn=Fee%20Payment&am=$amount&cu=INR";
 
-// Create QR code
-$qrCode = QrCode::create($upi_uri)
-    ->setEncoding(new Encoding('UTF-8'))
-    ->setErrorCorrectionLevel(ErrorCorrectionLevel::Medium)
-    ->setSize(300)
-    ->setMargin(10);
-
-$writer = new PngWriter();
-$result = $writer->write($qrCode);
+// Build QR code using Endroid v6
+$result = Builder::create()
+    ->writer(new PngWriter())
+    ->data($upi_uri)
+    ->encoding(new Encoding('UTF-8'))
+    ->errorCorrectionLevel(ErrorCorrectionLevel::Medium)
+    ->size(300)
+    ->margin(10)
+    ->build();
 
 // Output the QR code as a PNG image
 header('Content-Type: image/png');
