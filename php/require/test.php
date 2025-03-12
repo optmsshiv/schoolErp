ac2e304499688c8dc8a54290280e047fcce78d40
optmsshiv <optmsshiv@gmail.com>
2025-03-12 23:31:20 +0530

    chxk

--- php/require/test.php
+++ php/require/test.php
@@ -1,6 +1,8 @@
 <?php
 require $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
 
+use Endroid\QrCode\Builder\Builder;
+
 if (class_exists('Endroid\QrCode\Builder\Builder')) {
     echo "Builder class exists!";
 } else {
