e35943ae0291f719b4a5d931091a55bb5e1ef845
optmsshiv <optmsshiv@gmail.com>
2025-03-12 23:53:57 +0530

    "Update require/test.php to use Endroid\QrCode Builder class and enable error reporting."

--- php/require/test.php
+++ php/require/test.php
@@ -1,7 +1,14 @@
 <?php
 require $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
 
-use Endroid\QrCode\QrCode;
+use Endroid\QrCode\Builder\Builder;
+use Endroid\QrCode\Writer\PngWriter;
+use Endroid\QrCode\Encoding\Encoding;
+use Endroid\QrCode\ErrorCorrectionLevel;
 
-echo "QR Code library loaded successfully!";
+// Enable error reporting
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+echo "QR Code Builder class loaded successfully!";
 ?>
