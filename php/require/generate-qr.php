5d9946699004edb1ad9027f8b0dfe9bc186cb83f
optmsshiv <optmsshiv@gmail.com>
2025-03-13 20:57:29 +0530

    `Update generate-qr.php to use QrCode::class instead of Endroid\QrCode\QrCode`

--- php/require/generate-qr.php
+++ php/require/generate-qr.php
@@ -6,14 +6,13 @@ use Endroid\QrCode\Writer\PngWriter;
 use Endroid\QrCode\Encoding\Encoding;
 use Endroid\QrCode\ErrorCorrectionLevel;
 use Endroid\QrCode\RoundBlockSizeMode;
-use Endroid\QrCode\Writer\Result\PngResult;
 
 // Enable error reporting
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 // Check if the class exists
-if (!class_exists('Endroid\QrCode\QrCode')) {
+if (!class_exists(QrCode::class)) {
     die("Error: Endroid QR Code library is NOT loaded!");
 }
 
@@ -29,18 +28,18 @@ if ($amount <= 0) {
 $upi_uri = "upi://pay?pa=$upi_id&pn=School%20Fees&am=$amount&cu=INR";
 
 // Create QR code using correct v6 syntax
-$qrCode = new QrCode($upi_uri);
-$qrCode->setEncoding(new Encoding('UTF-8'));
-$qrCode->setErrorCorrectionLevel(ErrorCorrectionLevel::High);
-$qrCode->setSize(300);
-$qrCode->setMargin(10);
-$qrCode->setRoundBlockSizeMode(RoundBlockSizeMode::Margin);
+$qrCode = QrCode::create($upi_uri)
+    ->withEncoding(new Encoding('UTF-8'))
+    ->withErrorCorrectionLevel(ErrorCorrectionLevel::High)
+    ->withSize(300)
+    ->withMargin(10)
+    ->withRoundBlockSizeMode(RoundBlockSizeMode::Margin);
 
 // Generate QR code image
 $writer = new PngWriter();
 $result = $writer->write($qrCode);
 
-/** @var PngResult $result */
+// Set headers and output image
 header('Content-Type: image/png');
 echo $result->getString();
 ?>
