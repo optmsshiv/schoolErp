f717f16a42752bd6b165ad7d4433c188bcd5a3fe
optmsshiv <optmsshiv@gmail.com>
2025-03-13 12:01:34 +0530

    `Update test.php to display library load status with emojis`

--- php/require/test.php
+++ php/require/test.php
@@ -2,8 +2,8 @@
 require $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
 
 if (class_exists('Endroid\QrCode\QrCode')) {
-    echo "Endroid QR Code library is loaded!";
+    echo "✅ Endroid QR Code library is loaded!";
 } else {
-    echo "Error: Endroid QR Code library is NOT loaded!";
+    echo "❌ Error: Endroid QR Code library is NOT loaded!";
 }
 ?>
