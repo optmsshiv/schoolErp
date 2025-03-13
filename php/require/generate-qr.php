3b66f3053b8ec6f0ee082eaad797556fe591d4c6
optmsshiv <optmsshiv@gmail.com>
2025-03-13 20:51:18 +0530

    `Update generate-qr.php to use correct Endroid QR Code v6 syntax`

--- php/require/generate-qr.php
+++ php/require/generate-qr.php
@@ -6,7 +6,7 @@ use Endroid\QrCode\Writer\PngWriter;
 use Endroid\QrCode\Encoding\Encoding;
 use Endroid\QrCode\ErrorCorrectionLevel;
 use Endroid\QrCode\RoundBlockSizeMode;
-use Endroid\QrCode\Matrix\MatrixFactory;
+use Endroid\QrCode\Writer\Result\PngResult;
 
 // Enable error reporting
 error_reporting(E_ALL);
@@ -28,19 +28,19 @@ if ($amount <= 0) {
 // Generate the UPI payment URI
 $upi_uri = "upi://pay?pa=$upi_id&pn=School%20Fees&am=$amount&cu=INR";
 
-// Create QR code (Endroid QR Code v6 syntax)
-$qrCode = QrCode::create($upi_uri)
-    ->withEncoding(new Encoding('UTF-8'))
-    ->withErrorCorrectionLevel(ErrorCorrectionLevel::High)
-    ->withSize(300)
-    ->withMargin(10)
-    ->withRoundBlockSizeMode(RoundBlockSizeMode::Margin);
+// Create QR code using correct v6 syntax
+$qrCode = new QrCode($upi_uri);
+$qrCode->setEncoding(new Encoding('UTF-8'));
+$qrCode->setErrorCorrectionLevel(ErrorCorrectionLevel::High);
+$qrCode->setSize(300);
+$qrCode->setMargin(10);
+$qrCode->setRoundBlockSizeMode(RoundBlockSizeMode::Margin);
 
-// Generate QR code matrix and output image
+// Generate QR code image
 $writer = new PngWriter();
 $result = $writer->write($qrCode);
 
-// Set headers and output image
+/** @var PngResult $result */
 header('Content-Type: image/png');
 echo $result->getString();
 ?>
