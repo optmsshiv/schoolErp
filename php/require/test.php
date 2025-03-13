0c8f1cf94087f573169b490b9fb682310e9d3681
optmsshiv <optmsshiv@gmail.com>
2025-03-13 11:53:27 +0530

    "Updated test.php to display a different success message and error message for Endroid QR Code library loading."

--- php/require/test.php
+++ php/require/test.php
@@ -1,14 +1,9 @@
 <?php
 require $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
 
-// Enable error reporting
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-// Check if Endroid\QrCode\QrCode class exists
 if (class_exists('Endroid\QrCode\QrCode')) {
-    echo "Endroid QR Code library is properly loaded.";
+    echo "Endroid QR Code library is loaded!";
 } else {
-    echo "Endroid QR Code library is NOT loaded!";
+    echo "Error: Endroid QR Code library is NOT loaded!";
 }
 ?>
