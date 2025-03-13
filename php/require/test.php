fd3fba612e7c0c1442f72b8e72e66367b51ead99
optmsshiv <optmsshiv@gmail.com>
2025-03-13 10:10:44 +0530

    "Update Endroid QR Code library usage in generate-qr.php and test.php"

--- php/require/test.php
+++ php/require/test.php
@@ -1,9 +1,14 @@
 <?php
 require $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
 
-if (!class_exists('Endroid\QrCode\Builder\Builder')) {
-    die("Error: Endroid QR Code Builder class not found. Check composer installation.");
-}
+// Enable error reporting
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
-echo "QR Code Builder is available!";
+// Check if Endroid\QrCode\QrCode class exists
+if (class_exists('Endroid\QrCode\QrCode')) {
+    echo "Endroid QR Code library is properly loaded.";
+} else {
+    echo "Endroid QR Code library is NOT loaded!";
+}
 ?>
