9c91d3bec66349cc292d7860e33fcb487a56f605
optmsshiv <optmsshiv@gmail.com>
2025-03-13 23:53:41 +0530

    "Removed two blank lines from generate-qr.php"

--- php/require/generate-qr.php
+++ php/require/generate-qr.php
@@ -6,8 +6,6 @@ use Endroid\QrCode\Writer\PngWriter;
 use Endroid\QrCode\Encoding\Encoding;
 use Endroid\QrCode\ErrorCorrectionLevel;
 
-
-
 // Enable error reporting
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
