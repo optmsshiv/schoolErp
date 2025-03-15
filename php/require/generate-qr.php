54dcced97b3ce87f01d1e7483d10e38176a77dbf
optmsshiv <optmsshiv@gmail.com>
2025-03-15 13:00:18 +0530

    `Fixed UPI URI encoding in generate-qr.php`

--- php/require/generate-qr.php
+++ php/require/generate-qr.php
@@ -7,12 +7,12 @@ if ($amount <= 0) {
     die("Invalid amount");
 }
 
-// Generate the UPI payment URI
-$upi_uri = urlencode("upi://pay?pa=$upi_id&pn=School%20Fees&am=$amount&cu=INR");
+// Generate the UPI payment URI (properly encoded)
+$upi_uri = rawurlencode("upi://pay?pa=$upi_id&pn=School%20Fees&am=$amount&cu=INR");
 
 // Google Chart API QR Code URL
 $qr_url = "https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=$upi_uri&choe=UTF-8";
 
 // Output QR Code
-echo "<img src='$qr_url' alt='UPI QR Code'>";
+echo "<img src='$qr_url' alt='UPI QR Code' />";
 ?>
