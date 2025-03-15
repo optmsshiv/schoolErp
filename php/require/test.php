2e867a00bdcee34446df3e7a6b23ff934f5b140a
optmsshiv <optmsshiv@gmail.com>
2025-03-15 13:05:31 +0530

    `Update test.php to use urlencode instead of rawurlencode and quickchart.io instead of chart.googleapis.com`

--- php/require/test.php
+++ php/require/test.php
@@ -1,10 +1,9 @@
 <?php
-$amount = 100; // Example amount
-$upi_id = "yourupi@upi";  // Replace with your actual UPI ID
+$amount = 100;
+$upi_id = "yourupi@upi";
 
-$upi_uri = rawurlencode("upi://pay?pa=$upi_id&pn=School%20Fees&am=$amount&cu=INR");
-$qr_url = "https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=$upi_uri&choe=UTF-8";
+$upi_uri = urlencode("upi://pay?pa=$upi_id&pn=School%20Fees&am=$amount&cu=INR");
+$qr_url = "https://quickchart.io/qr?text=$upi_uri&size=300";
 
-echo "<p>QR URL: <a href='$qr_url' target='_blank'>$qr_url</a></p>";
 echo "<img src='$qr_url' alt='UPI QR Code' />";
 ?>
