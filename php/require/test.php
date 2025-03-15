dcaaddf9c9b91c43167ace65859b967e9fbf7f2a
optmsshiv <optmsshiv@gmail.com>
2025-03-15 13:02:12 +0530

    `Removed autoload and added UPI QR code generation`

--- php/require/test.php
+++ php/require/test.php
@@ -1,11 +1,10 @@
 <?php
-require $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
+$amount = 100; // Example amount
+$upi_id = "yourupi@upi";  // Replace with your actual UPI ID
 
-use Endroid\QrCode\Builder\Builder;
+$upi_uri = rawurlencode("upi://pay?pa=$upi_id&pn=School%20Fees&am=$amount&cu=INR");
+$qr_url = "https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=$upi_uri&choe=UTF-8";
 
-if (class_exists(Builder::class)) {
-    echo "Builder class is available!";
-} else {
-    echo "Builder class is NOT available!";
-}
+echo "<p>QR URL: <a href='$qr_url' target='_blank'>$qr_url</a></p>";
+echo "<img src='$qr_url' alt='UPI QR Code' />";
 ?>
