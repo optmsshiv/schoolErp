091a6ac6a06437f9c402b6f4fd0a224f93b171b1
optmsshiv <optmsshiv@gmail.com>
2025-03-15 13:07:11 +0530

    `Update test.php: change amount and UPI ID`

--- php/require/test.php
+++ php/require/test.php
@@ -1,6 +1,6 @@
 <?php
-$amount = 100;
-$upi_id = "yourupi@upi";
+$amount = 300;
+$upi_id = "7549033244@ybl";
 
 $upi_uri = urlencode("upi://pay?pa=$upi_id&pn=School%20Fees&am=$amount&cu=INR");
 $qr_url = "https://quickchart.io/qr?text=$upi_uri&size=300";
