b78bd3588154d46fd194eeae5e1b288a1cb44eca
optmsshiv <optmsshiv@gmail.com>
2025-03-15 21:39:26 +0530

    `Added UPI QR code generation using Endroid\QrCode library`

--- php/require/test.php
+++ php/require/test.php
@@ -1,13 +1,28 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+
+require $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
+
+use Endroid\QrCode\QrCode;
+use Endroid\QrCode\Writer\PngWriter;
+use Endroid\QrCode\Encoding\Encoding;
+use Endroid\QrCode\ErrorCorrectionLevel;
 
 header('Content-Type: image/png');
-$im = imagecreate(300, 300);
-$bg = imagecolorallocate($im, 255, 255, 255);
-$text_color = imagecolorallocate($im, 0, 0, 0);
-imagestring($im, 5, 75, 130, 'Test Image', $text_color);
-imagepng($im);
-imagedestroy($im);
+
+// Test UPI URI
+$upi_id = "yourupi@upi";
+$amount = isset($_GET['amount']) ? floatval($_GET['amount']) : 0;
+$upi_uri = "upi://pay?pa=" . urlencode($upi_id) . "&pn=" . urlencode("School Fees") . "&am=" . urlencode($amount) . "&cu=INR";
+
+$qrCode = QrCode::create($upi_uri)
+    ->setEncoding(new Encoding('UTF-8'))
+    ->setErrorCorrectionLevel(ErrorCorrectionLevel::High)
+    ->setSize(300)
+    ->setMargin(10);
+
+$writer = new PngWriter();
+$result = $writer->write($qrCode);
+
+// Output the QR Code image
+echo $result->getString();
 ?>
