a3931d0d79268076839821c6ce39fe8a1727ff31
optmsshiv <optmsshiv@gmail.com>
2025-03-20 14:55:26 +0530

    `Update require path in test.php to point to specific vendor directory`

--- php/require/test.php
+++ php/require/test.php
@@ -1,7 +1,8 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-require __DIR__ . '/vendor/autoload.php';  // Adjust based on your project structure
+require '/home1/edrppymy/public_html/erp/vendor/autoload.php';
+  // Adjust based on your project structure
 
 
 use Endroid\QrCode\QrCode;
