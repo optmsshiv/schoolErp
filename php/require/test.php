ac891df61ccbae6b67629b48e555ca340de340b6
optmsshiv <optmsshiv@gmail.com>
2025-03-20 14:52:20 +0530

    `Added Endroid\QrCode library and generated QR code in test.php`

--- php/require/test.php
+++ php/require/test.php
@@ -1,6 +1,17 @@
 <?php
+require '/vendor/autoload.php';
 
-phpinfo();
+use Endroid\QrCode\QrCode;
+use Endroid\QrCode\Writer\PngWriter;
 
+$qrCode = QrCode::create('Hello from Live Server!')
+    ->setSize(300)
+    ->setMargin(10);
 
+$writer = new PngWriter();
+$result = $writer->write($qrCode);
+
+// Output as PNG
+header('Content-Type: '.$result->getMimeType());
+echo $result->getString();
 ?>
