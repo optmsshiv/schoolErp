62c3dd5dc4d364a7dbe0c0d3e81a0d0cba032791
optmsshiv <optmsshiv@gmail.com>
2025-03-20 14:54:09 +0530

    `Update require statement to use __DIR__ for absolute path`

--- php/require/test.php
+++ php/require/test.php
@@ -1,7 +1,8 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-require '/vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';  // Adjust based on your project structure
+
 
 use Endroid\QrCode\QrCode;
 use Endroid\QrCode\Writer\PngWriter;
