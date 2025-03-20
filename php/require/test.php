5edea01f9c00f2c111f64cab58384b4ca2358bae
optmsshiv <optmsshiv@gmail.com>
2025-03-20 14:57:16 +0530

    `Update Endroid\QrCode usage to create QR code with specific settings`

--- php/require/test.php
+++ php/require/test.php
@@ -6,16 +6,21 @@ require '/home1/edrppymy/public_html/erp/vendor/autoload.php';
 
 
 use Endroid\QrCode\QrCode;
+use Endroid\QrCode\Encoding\Encoding;
+use Endroid\QrCode\ErrorCorrectionLevel;
+use Endroid\QrCode\Label\Label;
 use Endroid\QrCode\Writer\PngWriter;
 
-$qrCode = new QrCode('Hello from Live Server!');
-$qrCode->setSize(300);
-$qrCode->setMargin(10);
+$qrCode = QrCode::create('Hello from Live Server!')
+    ->setEncoding(new Encoding('UTF-8'))
+    ->setErrorCorrectionLevel(ErrorCorrectionLevel::High)
+    ->setSize(300)
+    ->setMargin(10);
 
 $writer = new PngWriter();
 $result = $writer->write($qrCode);
 
 // Output as PNG
-header('Content-Type: '.$result->getMimeType());
+header('Content-Type: ' . $result->getMimeType());
 echo $result->getString();
 ?>
