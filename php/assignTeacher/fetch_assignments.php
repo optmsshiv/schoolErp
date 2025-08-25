0185381b9208a613ef45506894135abdeeba5bc7
optmsshiv <optmsshiv@gmail.com>
2025-08-26 01:39:54 +0530

    fetch load

--- php/assignTeacher/fetch_assignments.php
+++ php/assignTeacher/fetch_assignments.php
@@ -2,6 +2,7 @@
 global $pdo;
 require '../db_connection.php';
 
+
 $sql = "SELECT ct.id, c.class_name, u.fullname AS teacher_name,
                ct.assigned_date, ct.updated_at
         FROM class_teachers ct
