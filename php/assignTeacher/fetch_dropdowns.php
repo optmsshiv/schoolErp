3df8f47ee99c12ee526f1644990e5305db582719
optmsshiv <optmsshiv@gmail.com>
2025-08-26 01:40:59 +0530

    fetch dropdown updated

--- php/assignTeacher/fetch_dropdowns.php
+++ php/assignTeacher/fetch_dropdowns.php
@@ -3,10 +3,10 @@ global $pdo;
 require '../db_connection.php'; // your PDO connection
 
 // Fetch classes
-$classes = $pdo->query("SELECT class_id, class_name FROM classes")->fetchAll(PDO::FETCH_ASSOC);
+$classes = $pdo->query("SELECT class_id, class_name FROM Classes")->fetchAll(PDO::FETCH_ASSOC);
 
 // Fetch teachers (only role = teacher)
-$teachers = $pdo->query("SELECT id, fullname FROM userrole WHERE role = 'Teacher'")->fetchAll(PDO::FETCH_ASSOC);
+$teachers = $pdo->query("SELECT id, fullname FROM userRole WHERE role = 'Teacher'")->fetchAll(PDO::FETCH_ASSOC);
 
 echo json_encode([
   "classes" => $classes,
