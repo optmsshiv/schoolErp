ac8901b6d6c1a2d25ce050da13bf3ab45483dba4
optmsshiv <optmsshiv@gmail.com>
2025-08-26 01:42:29 +0530

    fetch dropdown updated

--- php/assignTeacher/fetch_assignments.php
+++ php/assignTeacher/fetch_assignments.php
@@ -6,8 +6,8 @@ require '../db_connection.php';
 $sql = "SELECT ct.id, c.class_name, u.fullname AS teacher_name,
                ct.assigned_date, ct.updated_at
         FROM class_teachers ct
-        JOIN classes c ON ct.class_id = c.class_id
-        JOIN userrole u ON ct.teacher_id = u.id";
+        JOIN Classes c ON ct.class_id = c.class_id
+        JOIN userRole u ON ct.teacher_id = u.id";
 $result = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 
 echo json_encode($result);
