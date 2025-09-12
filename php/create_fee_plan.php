d616c007aae9000821a7e3e80e1c8e1f378b50b4
optmsshiv <optmsshiv@gmail.com>
2025-09-12 08:57:34 +0530

    create fee plan php updated

--- php/create_fee_plan.php
+++ php/create_fee_plan.php
@@ -3,11 +3,11 @@ global $pdo;
 include '../php/db_connection.php'; // your PDO connection
 
 // Fetch Fee Heads
-$feeHeadsStmt = $pdo->query("SELECT fee_head_id, fee_head_name FROM feeheads ORDER BY fee_head_name ASC");
+$feeHeadsStmt = $pdo->query("SELECT fee_head_id, fee_head_name FROM FeeHeads ORDER BY fee_head_name ASC");
 $feeheads = $feeHeadsStmt->fetchAll(PDO::FETCH_ASSOC);
 
 // Fetch Classes
-$classesStmt = $pdo->query("SELECT class_id, class_name FROM classes ORDER BY class_name ASC");
+$classesStmt = $pdo->query("SELECT class_id, class_name FROM Classes ORDER BY class_name ASC");
 $classes = $classesStmt->fetchAll(PDO::FETCH_ASSOC);
 
 echo json_encode([
