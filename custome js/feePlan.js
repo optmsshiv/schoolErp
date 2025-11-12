236e9cbc34be0868a38e44ec915e1664952df0dd
optmsshiv <optmsshiv@gmail.com>
2025-11-12 21:00:26 +0530

    error checking added

--- custome js/feePlan.js
+++ custome js/feePlan.js
@@ -195,6 +195,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
       if (!confirm("Are you sure you want to delete this fee plan?")) return;
 
+      console.log("Deleting fee plan ID:", feePlanId);
+
+
       fetch("../php/feePlan/delete_fee_plan.php", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
