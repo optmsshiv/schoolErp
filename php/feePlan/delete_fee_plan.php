8fb06705db9619a48aeabdcbde6efc89ba946523
optmsshiv <optmsshiv@gmail.com>
2025-11-12 20:53:34 +0530

    error checking added

--- php/feePlan/delete_fee_plan.php
+++ php/feePlan/delete_fee_plan.php
@@ -1,5 +1,9 @@
 <?php
 global $pdo;
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
 header('Content-Type: application/json');
 include '../db_connection.php'; // adjust path as needed
 
